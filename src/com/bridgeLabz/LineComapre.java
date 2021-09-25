374614f94252331012f95bf049ec6cca4e7b8297
sngtmth <sngtmth@gmail.com>
2021-09-25 16:02:51 +0530

    welcome Message

---
+++ src/com/bridgeLabz/LineComapre.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+public class LineComapre {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparision computation on master branch");
+
+	}
+
+}
